953ba87a75d78d8a0a85c4c368bfc542f28ee957
nekitrasha <nikaegovo@gmail.com>
2026-09-02 10:47:14 +0300

    Update AEZProjectile.h

--- Source/EchoZone/Weapon/AEZProjectile.h
+++ Source/EchoZone/Weapon/AEZProjectile.h
@@ -7,7 +7,6 @@
 #include "AEZProjectile.generated.h"
 
 class UStaticMeshComponent;
-class UProjectileMovementComponent;
 class USphereComponent;
 class UEZAmmoDataAsset;
 
@@ -23,16 +22,14 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
 
 protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	USphereComponent* CollisionComponent;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
-	UStaticMeshComponent* MeshComponent;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
-	UProjectileMovementComponent* ProjectileMovement;
+	UStaticMeshComponent* ProjectileMesh;
 
 	UPROPERTY(BlueprintReadOnly, Category = "Ammo")
 	TObjectPtr<const UEZAmmoDataAsset> AmmoData;
