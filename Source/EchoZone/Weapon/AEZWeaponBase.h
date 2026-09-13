6974007f06e7e5b8eb7a42d9104f14c946a5a7d3
nekitrasha <nikaegovo@gmail.com>
2026-09-13 11:31:41 +0300

    Переработка WeaponSystem

--- Source/EchoZone/Weapon/AEZWeaponBase.h
+++ Source/EchoZone/Weapon/AEZWeaponBase.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -15,48 +13,51 @@
 #include "AEZWeaponBase.generated.h"
 
 class UStaticMeshComponent;
-class USceneRootComponent;
+class USceneComponent;
 class AEZProjectile;
 class UEZAmmoDataAsset;
 class UEZWeaponDataAsset;
 class UEZMagazineDataAsset;
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FEZWeaponSimpleSignature);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FEZWeaponReloadStartedSignature, bool, bTacticalReload);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FEZWeaponFireModeChangedSignature, EEZFireMode, NewMode);
+
 UCLASS()
 class ECHOZONE_API AEZWeaponBase : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
+
+public:
 	AEZWeaponBase();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaSeconds) override;
 
 protected:
+	virtual void BeginPlay() override;
+
+protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
-	USceneComponent* RootScene;
-	
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
-	USceneComponent* WeaponVisualRoot;
+	TObjectPtr<USceneComponent> RootScene;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
-	UStaticMeshComponent* WeaponMesh;
+	TObjectPtr<USceneComponent> WeaponVisualRoot;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
-	USceneComponent* MuzzlePoint;
+	TObjectPtr<UStaticMeshComponent> WeaponMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<USceneComponent> MuzzlePoint;
 
 protected:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon")
 	TSubclassOf<AEZProjectile> ProjectileClass;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon")
-	TObjectPtr<UEZWeaponDataAsset> WeaponData;
+	TObjectPtr<UEZWeaponDataAsset> WeaponData = nullptr;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon")
-	TObjectPtr<UEZAmmoDataAsset> CurrentAmmoData;
+	TObjectPtr<UEZAmmoDataAsset> CurrentAmmoData = nullptr;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon")
 	EEZFireMode CurrentFireMode = EEZFireMode::SemiAuto;
@@ -74,7 +75,7 @@ protected:
 	bool bIsReloading = false;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon")
-	bool bTriggerHeld = false;
+	bool bTriggeredHeld = false;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon")
 	bool bIsAiming = false;
@@ -83,7 +84,7 @@ protected:
 	bool bWasEmptyClick = false;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon|State")
-	int32 BurstShotsRemaining = 0;
+	int32 BurstShotRemaining = 0;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon|State")
 	bool bTacticalReloadInProgress = false;
@@ -97,31 +98,30 @@ protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon")
 	float CurrentAimAlpha = 0.0f;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon|Visual")
-	FVector VisualLocationOffset = FVector::ZeroVector;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon|Visual")
-	FRotator VisualRotationOffset = FRotator::ZeroRotator;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon|Visual")
-	FVector TargetVisualLocationOffset = FVector::ZeroVector;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon|Visual")
-	FRotator TargetVisualRotationOffset = FRotator::ZeroRotator;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon|Visual")
-	FVector SwayLocationOffset = FVector::ZeroVector;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon|Visual")
-	FRotator SwayRotationOffset = FRotator::ZeroRotator;
-	
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Debug")
 	bool bDrawDebugShot = false;
 
 	FTimerHandle AutoFireTimerHandle;
 	FTimerHandle ReloadTimerHandle;
+	FTimerHandle EmptyClickCooldownHandle;
 
-public:	
+public:
+	UPROPERTY(BlueprintAssignable, Category = "Weapon|Events")
+	FEZWeaponSimpleSignature OnShotFiredEvent;
+
+	UPROPERTY(BlueprintAssignable, Category = "Weapon|Events")
+	FEZWeaponSimpleSignature OnEmptyClickEvent;
+
+	UPROPERTY(BlueprintAssignable, Category = "Weapon|Events")
+	FEZWeaponReloadStartedSignature OnReloadStartedEvent;
+
+	UPROPERTY(BlueprintAssignable, Category = "Weapon|Events")
+	FEZWeaponSimpleSignature OnReloadFinishedEvent;
+
+	UPROPERTY(BlueprintAssignable, Category = "Weapon|Events")
+	FEZWeaponFireModeChangedSignature OnFireModeChangedEvent;
+
+public:
 	UFUNCTION(BlueprintCallable, Category = "Weapon")
 	virtual void StartFire();
 
@@ -158,109 +158,5 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "Weapon")
 	USceneComponent* GetWeaponVisualRoot() const { return WeaponVisualRoot; }
 
-	UFUNCTION(BlueprintCallable, Category = "Weapon")
-	EEZWeaponType GetWeaponType() const;
 
-	UFUNCTION(BlueprintCallable, Category = "Weapon")
-	EEZAmmoCaliber GetWeaponCaliber() const;
-
-	UFUNCTION(BlueprintCallable, Category = "Weapon")
-	EEZReloadType GetReloadType() const;
-
-	UFUNCTION(BlueprintCallable, Category = "Weapon")
-	EEZWeaponFeedType GetFeedType() const;
-
-	UFUNCTION(BlueprintCallable, Category = "Weapon|Fire")
-	EEZFireMode GetCurrentFireMode() const { return CurrentFireMode; }
-
-	UFUNCTION(BlueprintCallable, Category = "Weapon")
-	bool IsMagazineCompatible(const UEZMagazineDataAsset* MagazineData) const;
-
-	UFUNCTION(BlueprintCallable, Category = "Weapon")
-	bool IsAmmoCompatible(const UEZAmmoDataAsset* AmmoData) const;
-
-	UFUNCTION(BlueprintCallable, Category = "Weapon")
-	int32 GetCurrentMagazineAmmo() const;
-
-	UFUNCTION(BlueprintCallable, Category = "Weapon")
-	int32 GetAmmoReadyToFire() const;
-
-	UFUNCTION(BlueprintCallable, Category = "Weapon")
-	bool HasInsertedMagazine() const;
-
-	UFUNCTION(BlueprintCallable, Category = "Weapon")
-	bool HasSpareMagazine() const;
-
-	UFUNCTION(BlueprintCallable, Category = "Weapon|Fire")
-	void SwitchFireMode();
-
-	UFUNCTION(BlueprintCallable, Category = "Weapon|Ammo")
-	bool HasRoundChambered() const { return bRoundChambered; }
-
-	UFUNCTION(BlueprintImplementableEvent, Category = "Weapon|FX")
-	void BP_OnEmptyClick();
-
-	UFUNCTION(BlueprintImplementableEvent, Category = "Weapon|FX")
-	void BP_OnReloadStarted(bool bTacticalReload);
-
-	UFUNCTION(BlueprintImplementableEvent, Category = "Weapon|FX")
-	void BP_OnReloadCanceled();
-
-	UFUNCTION(BlueprintImplementableEvent, Category = "Weapon|FX")
-	void BP_OnReloadFinished();
-
-	UFUNCTION(BlueprintImplementableEvent, Category = "Weapon|FX")
-	void BP_OnFireModeChanged(EEZFireMode NewMode);
-
-	UFUNCTION(BlueprintImplementableEvent, Category = "Weapon|FX")
-	void BP_OnVisualRecoil();
-
-	UFUNCTION(BlueprintImplementableEvent, Category = "Weapon|FX")
-	void BP_OnShotFired();
-
-protected:
-	void InitializeFromData();
-	void FireShot();
-	void HandleAutoFire();
-	void FinishReload();
-	void ConsumeRound();
-
-	void HandleFireModeShot();
-	void HandleBurstProgress();
-	bool CanShootCurrentMode() const;
-	bool IsFireModeSupported(EEZFireMode FireMode) const;
-
-	void PlayEmptyClick();
-	bool IsTacticalReload() const;
-	bool NeedsChamberingAfterReload() const;
-
-	void UpdateVisualRecoil(float DeltaSeconds);
-	void AddVisualRecoil();
-	void UpdateWeaponSway(float DeltaSeconds);
-	void UpdateADSAlignment(float DeltaSeconds);
-	
-	void UpdateSpread(float DeltaSeconds);
-	void UpdateRecoil(float DeltaSeconds);
-	void UpdateADS(float DeltaSeconds);
-	void UpdateVisualOffset(float DeltaSeconds);
-
-	FVector GetCameraAimPoint() const;
-	FVector GetShotDirection(const FVector& FromLocation) const;
-
-	void ApplyRecoil();
-
-	float GetErgonomicsNormalized() const;
-	float GetMovementSpreadMultiplier() const;
-	float GetEffectiveBaseSpread() const;
-	float GetEffectiveVerticalRecoil() const;
-	float GetEffectiveHorizontalRecoil() const;
-	bool IsOwnerSprinting() const;
-
-	int32 GetMagazineCapacity() const;
-	float GetReloadDuration()  const;
-
-	bool TryChamberNextRound();
-	int32 FindBestMagazineIndex() const;
-	void InsertMagazine(const FEZMagazineInstance& NewMagazine);
-	FEZMagazineInstance RemoveInsertedMagazine();
 };
